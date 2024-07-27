b49531aa79b2e5fc906dd8179ebd198f6c942305
Heiland <germansh21@gmail.com>
2024-07-27 01:21:33 -0300

    Add files via upload

--- Guia28.java
+++ Guia28.java
@@ -3,7 +3,7 @@ package unidad2;
 public class Guia28 {
     public static void main(String[] args) {
         int i;
-        i=0;
+        i=1;
         while (i<5) {
             System.out.println("10");
             i = i + 1;
